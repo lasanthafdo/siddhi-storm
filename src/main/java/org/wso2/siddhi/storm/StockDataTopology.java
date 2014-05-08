12ef251ebd726ff38bfed84bb0ee256ccefb4d17
Lasantha Fernando <lasantha.fdo@gmail.com>
2014-05-08 11:45:09 +0530

    Improvements to Stock topology

--- src/main/java/org/wso2/siddhi/storm/StockDataTopology.java
+++ src/main/java/org/wso2/siddhi/storm/StockDataTopology.java
@@ -1,11 +1,9 @@
 package org.wso2.siddhi.storm;
 
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 import org.wso2.siddhi.storm.components.EchoBolt;
-import org.wso2.siddhi.storm.components.FootballDataSpout;
 import org.wso2.siddhi.storm.components.SiddhiBolt;
 import org.wso2.siddhi.storm.components.StockDataSpout;
 
@@ -16,19 +14,17 @@ public class StockDataTopology {
 
 
     private static SiddhiBolt configureSiddhiBolt1() {
-        SiddhiBolt siddhiBolt = new SiddhiBolt(
+        return new SiddhiBolt(
                 new String[]{"define stream StockData ( symbol string, price double, volume int);"},
                 new String[]{"from StockData#window.time(1sec) select symbol, price, avg(volume) as avgV group by symbol insert into AvgVolume;"},
                 new String[]{"AvgVolume"});
-        return siddhiBolt;
     }
 
     private static SiddhiBolt configureSiddhiBolt2() {
-        SiddhiBolt siddhiBolt = new SiddhiBolt(
+        return new SiddhiBolt(
                 new String[]{"define stream AvgRunPlay ( sid string, v double);"},
                 new String[]{"from AvgRunPlay[v>20] select sid, v as v insert into FastRunPlay;"},
                 new String[]{"FastRunPlay"});
-        return siddhiBolt;
     }
 
     public static void main(String[] args) throws Exception {
@@ -41,14 +37,7 @@ public class StockDataTopology {
 
         Config conf = new Config();
 
-
-        if (args != null && args.length > 0) {
-            conf.setNumWorkers(3);
-            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-        } else {
-            conf.setMaxTaskParallelism(3);
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("stock-analysis", conf, builder.createTopology());
-        }
+        conf.setNumWorkers(3);
+        StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
     }
 }
